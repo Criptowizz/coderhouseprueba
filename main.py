d0b2be0573d6ced1afe63ffa1d098474aad64722
Antonio Martinez <antonio-1884@hotmail.com>
2023-11-01 22:04:31 -0300

    haciendo cambios

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 # Press Mayús+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-x = 3
+x = 33
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
